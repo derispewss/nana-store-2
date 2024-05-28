8e79dd6b72d71445e7151ef1d00d28f89620ce63
derispewss <derisfirmansyah177@gmail.com>
2024-05-29 01:03:01 +0700

    Remove secret and fix configuration

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin')
 const configAccount = require('./config-firebase.json')
 
 admin.initializeApp({
-    credential: admin.credential.cert(configAccount),
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
     storageBucket: 'gs://nana-store-c5456.appspot.com'
 })
 
