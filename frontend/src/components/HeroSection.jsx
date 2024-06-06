50a3c453cabf5fef9f3b488dfa594f7317c048c6
derispewss <derisfirmansyah177@gmail.com>
2024-06-06 21:47:35 +0700

    update hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
-    const EndpointUrl = 'https://api.storenana.my.id/products';
+    const EndpointUrl = 'https://haras-store-backend.vercel.app/products';
     const [data, setData] = useState([]);
     const [groupedData, setGroupedData] = useState({});
     
@@ -63,29 +63,37 @@ const HeroSection = () => {
                 {Object.entries(groupedData).length > 0 ? (
                     Object.entries(groupedData).map(([category, items]) => (
                         <div key={category} className="card rounded-md p-4 my-4">
-                            <h2 className="m-5 px-3 py-2 bg-gray-100 border-l-4 border-l-yellow-950 font-semibold">{category}</h2>
+                            <h2 className="m-5 px-3 py-2 bg-meron border-l-4 border-l-black font-semibold text-white">{category}</h2>
                             <section id={`category-${category.toLowerCase()}`} className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-4">
                                 {items.map((item) => (
-                                <div key={item.id} className="rounded-lg duration-300 hover:scale-105 overflow-hidden text-center bg-gray-200 border shadow-md flex flex-col">
-                                    <Link to={`/order/${item.slug}`}>
-                                        <div className="overflow-hidden">
-                                            <img className='w-full h-45 object-cover rounded-t-md transition-transform duration-300 hover:scale-110' src={item.logo || 'https://placehold.co/500x500/png'} alt={item.name} />
-                                        </div>
-                                        <div className="md:py-4 flex-col flex justify-center items-center">
-                                            <div className="text-sm md:text-sm font-semibold text-gray-800 mb-2">{item.name}</div>
-                                        </div>
-                                    </Link>
-                                </div>
+                                    <ProductCard key={item.id} item={item} />
                                 ))}
                             </section>
                         </div>
                     ))
                 ) : (
-                    <div className="text-center bg-white rounded-lg p-4 m-4">Maaf, product belum ditambahkan untuk saat ini.</div>
+                    <div className="text-center bg-white rounded-lg p-4 m-4">Maaf, produk belum ditambahkan untuk saat ini.</div>
                 )}
             </main>
         </div>
-    )
+    );
 }
+
+// ProductCard Component
+const ProductCard = ({ item }) => {
+    const { slug, logo, name } = item;
+    return (
+        <div className="rounded-lg duration-300 hover:scale-105 overflow-hidden text-center bg-gray-200 border shadow-md flex flex-col">
+            <Link to={`/order/${slug}`}>
+                <div className="overflow-hidden">
+                    <img className='w-full h-45 object-cover rounded-t-md transition-transform duration-300 hover:scale-110' src={logo || 'https://placehold.co/500x500/png'} alt={name} />
+                </div>
+                <div className="md:py-4 flex-col flex justify-center items-center">
+                    <div className="text-sm md:text-sm font-semibold text-gray-800 mb-2">{name}</div>
+                </div>
+            </Link>
+        </div>
+    );
+};
 
 export default HeroSection;
